c78da05fd2f6400f184d0977b28baf4ac0fff07b
Jimmy Cohen-Boulakia <jcohenboulakia@docxa.com>
2015-06-09 17:27:29 +0200

    Added square & round shape

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ var c = $('#myCanvas')[0];
 // console.log(c);
 // console.log(c1);
 
-var draw = false;
+var drawing = false;
 var color = 'black';
 var shape = 'round';
+var x = 0, y = 0;
 
 
 
 $('table#colorSelector td').on('click', function(event){
 	color = event.target.id;
+});
+
+$('table#shapeSelector td').on('click', function(event){
+	shape = event.target.id;
 });
 
 var drawRound = function (x, y, size, color) {
@@ -27,63 +32,39 @@ var drawRound = function (x, y, size, color) {
 var drawSquare = function (x, y, size, color) {
 	var ctx = c.getContext("2d");
 	ctx.fillStyle = color;
-	ctx.fillRect(x, y, size, size);
+	ctx.fillRect(x, y, size* Math.PI/2, size* Math.PI/2);
 };
 
-var shapes = ['roud' : drawRound, 'square' : drawSquare];
-
-window.onmousedown = function() {
-	draw = true;
+var draw = function () {
+	if (shape === 'round') {
+		drawRound(x, y, 12, color);
+	}
+	else if (shape === 'square') {
+		drawSquare(x, y, 12, color);
+	}
 };
 
-window.onmouseup = function() {
-	draw = false;
+
+c.onmousedown = function() {
+	drawing = true;
+	x = event.offsetX;
+	y = event.offsetY;
+	draw();
 };
+
+c.onmouseup = function() {
+	drawing = false;
+	x = event.offsetX;
+	y = event.offsetY;
+};
+
 
 
 c.onmousemove = function(event) {
-	if (draw) {
-		// console.log(event);
-		var x = event.offsetX, y = event.offsetY;
-		var ctx = c.getContext("2d");
-		ctx.fillStyle = "#FF0000";
-		ctx.strokeStyle = '#FF0000';
-		// ctx.fillRect(x, y, 25, 25);
-
-		// window.setTimeout(function(){
-		// 	ctx.clearRect ( x , y, 25, 25);
-		// }, 1000);
-		ctx.beginPath();
-		ctx.arc(x, y, 12, 0, 2 * Math.PI, false);
-		ctx.fill();
-		ctx.stroke();
-
-		// window.setTimeout(function(){
-		// 	ctx.fillStyle = "green";
-		// 	ctx.fillRect(x, y, 16, 16);
-		// }, 1200);
-
-		// window.setTimeout(function(){
-		// 	ctx.clearRect ( x , y, 16, 16);
-		// }, 1400);
-
-		// window.setTimeout(function(){
-		// 	ctx.fillStyle = "blue";
-		// 	ctx.fillRect(x, y, 12, 12);
-		// }, 1600);
-
-		// window.setTimeout(function(){
-		// 	ctx.clearRect ( x , y, 12, 12);
-		// }, 1800);
-
-		// window.setTimeout(function(){
-		// 	ctx.fillStyle = color;
-		// 	ctx.fillRect(x, y, 8, 8);
-		// }, 2000);
-
-		// window.setTimeout(function(){
-		// 	ctx.clearRect ( x , y, 25, 25);
-		// }, 2200);
+	x = event.offsetX;
+	y = event.offsetY;
+	if(drawing){
+		draw();
 	}
 
 };
